41ac7818f59abdd6810d53fcdb567bef8da71f27
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-04-15 09:19:59 +0900

    material-uiのスタイル変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css';
-import {AnswersList, Chats} from './components/index'
+import { AnswersList, Chats } from './components/index'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,41 +12,58 @@ export default class App extends React.Component {
       currentId: 'init',
       dataset: defaultDataset,
       open: false,
-    };
+    }
+    this.selectAnswer = this.selectAnswer.bind(this);
   }
 
-  initAnswer = () => {
-    const initDataset = this.state.dataset[this.state.currentId ];
-    const initAnswers = initDataset.answers;
-    this.setState({
-      answers: initAnswers
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
     })
-  }
-  initChats = () => {
-    const initDataset = this.state.dataset[this.state.currentId ];
-    const chat = {
-      text: initDataset.question,
-     type: 'question',
-      }
-
-      const chats = this.state.chats;
-      chats.push(chat)
 
     this.setState({
-      chats: chats
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
     })
   }
 
-  componentDidMount(){
-    this.initChats();
-    this.initAnswer();
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch (true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId)
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+
+        this.setState({
+          chats: chats
+        })
+
+        this.displayNextQuestion(nextQuestionId)
+        break;
+    }
   }
-  
+
+  componentDidMount() {
+    const initAnswer = ""
+    this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
   render() {
     return <section className='c-section'>
       <div className='c-box'>
         <Chats chats={this.state.chats} />
-        <AnswersList answers={this.state.answers} />
+        {console.log(this.state.chats)}
+        {console.log(this.state.answers)}
+        <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+        
       </div>
     </section>;
   }
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import List from '@material-ui/core/List';
 import {Chat} from "./index";
 //↑{}名前付きimport
+const useStyles = makeStyles(() => (
+    createStyles({
+        "chats": {
+            height: 400,
+            padding: '0',
+            overflow: 'auto'
+        }
+    })
+));
 
 const Chats = (props) => {
+    const classes = useStyles();
+
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List className={classes.chats}>
             {props.chats.map((chat, index) =>  {
-                return <Chat text={chat.text} type={chat.type} key= {index.toString()} />
+                return <Chat text={chat.text} type={chat.type} key={index.toString()} />
             })}
         </List>
     )
